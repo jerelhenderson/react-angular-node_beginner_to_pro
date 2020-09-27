674373304f6d2720e86b2ebd98a8f1a1fba15f55
Jerel Henderson <henderson.jerel@yahoo.com>
2020-09-27 07:36:23 +0000

    Switch to SASS files

--- src/app/temp/temp.component.ts
+++ src/app/temp/temp.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-temp',
   templateUrl: './temp.component.html',
-  styleUrls: ['./temp.component.css']
+  styleUrls: ['./temp.component.scss']
 })
 export class TempComponent implements OnInit {
 
