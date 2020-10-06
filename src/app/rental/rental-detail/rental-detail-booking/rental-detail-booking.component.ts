adb804ad62f30326a99d3f3d1e12cb521eb5c7f6
jerelhenderson <henderson.jerel@yahoo.com>
2020-10-06 13:25:16 +0000

    Add new component + styling and formatting

---
+++ src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'bwm-rental-detail-booking',
+    templateUrl: './rental-detail-booking.component.html',
+    styleUrls: ['./rental-detail-booking.component.scss']
+})
+export class RentalDetailBookingComponent implements OnInit {
+    @Input() price: number;
+
+    public daterange: any = {};
+
+    // see original project for full list of options
+    // can also be setup using the config service to apply to multiple pickers
+    public options: any = {
+        locale: { format: 'YYYY-MM-DD' },
+        alwaysShowCalendars: false,
+        opens: 'left'
+    };
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    public selectedDate(value: any, datepicker?: any) {
+        // this is the date selected
+        console.log(value);
+
+        // any object can be passed to the selected event and it will be passed back here
+        datepicker.start = value.start;
+        datepicker.end = value.end;
+
+        // use passed valuable to update state
+        this.daterange.start = value.start;
+        this.daterange.end = value.end;
+        this.daterange.label = value.label;
+    }
+}
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from "@angular/common/http";
 import { NgPipesModule } from 'ngx-pipes';
 import { MapModule } from '../common/map/map.module';
+import { Daterangepicker } from 'ng2-daterangepicker';
 
 import { RentalComponent } from './rental.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
@@ -12,6 +13,7 @@ import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 
 import { RentalService } from "./shared/rental.service";
 import { AuthGuard } from "../auth/shared/auth.guard";
+import { RentalDetailBookingComponent } from './rental-detail/rental-detail-booking/rental-detail-booking.component';
 
 const routes: Routes = [
     {path: 'rentals',
@@ -28,13 +30,15 @@ const routes: Routes = [
         RentalListComponent,
         RentalListItemComponent,
         RentalDetailComponent,
+        RentalDetailBookingComponent,
     ],
     imports: [
         CommonModule,
         RouterModule.forChild(routes),
         HttpClientModule,
         NgPipesModule,
-        MapModule
+        MapModule,
+        Daterangepicker
     ],
     providers: [
         RentalService
