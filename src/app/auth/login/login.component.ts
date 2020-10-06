c77fd92e4e3e68f0e2e3c7ba965ca9954a3f8bd0
jerelhenderson <henderson.jerel@yahoo.com>
2020-10-06 06:36:39 +0000

    Add images + "login successful" message

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../shared/auth.service';
 
 @Component({
@@ -11,11 +11,18 @@ import { AuthService } from '../shared/auth.service';
 export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     errors: any[] = [];
+    notifyMessage: string = '';
 
-    constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
+    constructor(private fb: FormBuilder, private authService: AuthService, private router: Router, private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.initForm();
+
+        this.route.params.subscribe((params) => {
+            if (params['registered'] === 'Success') {
+                this.notifyMessage = "You have successfully registered. Please login now!";
+            }
+        })
     }
 
     initForm() {
