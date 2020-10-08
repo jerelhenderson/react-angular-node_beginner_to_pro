1643f8dd65984ba9ea52ed7da8fc4110cf0bd151
jerelhenderson <henderson.jerel@yahoo.com>
2020-10-08 11:04:57 +0000

    Add query feature + refactor Rentals HTTP routing

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -2,19 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Rental = require('../models/rental');
 
-const UserController = require('../controllers/user')
+const UserController = require('../controllers/user');
+
+const { normalizeErrors } = require('../helpers/mongoose');
 
 router.get('/secret', UserController.authMiddleware, function(req, res) {
     res.json({"secret": true})
-})
-
-router.get('', (req, res) => {
-    Rental.find({})
-            .select('-bookings')
-            .exec(function(err, foundRentals) {
-
-        res.json(foundRentals);
-    });
 });
 
 router.get('/:id', (req, res) => {
@@ -27,6 +20,25 @@ router.get('/:id', (req, res) => {
             return res.status(422).send({errors: [{"title": 'Rental Error', "detail": 'Could not find rental!'}]});
         }
         return res.json(foundRental)
+    });
+});
+
+router.get('', (req, res) => {
+    const city = req.query.city;
+    const query = city ? {city: city.toLowerCase()} : {};
+
+    Rental.find(query)
+        .select('-bookings')
+        .exec(function(err, foundRentals) {
+    if (err) {
+        return res.status(422).send({errors: normalizeErrors(err.errors)});
+    }
+
+    if (city && foundRentals.length === 0) {
+        return res.status(422).send({errors: [{"title": 'No Rentals Found', "detail": `There are no rentals in ${city}`}]});
+    }
+
+    return res.json(foundRentals);
     });
 });
 
