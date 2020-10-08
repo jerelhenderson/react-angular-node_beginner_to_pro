82f47204c9884337b9609acd6cfc9ed2538d209f
jerelhenderson <henderson.jerel@yahoo.com>
2020-10-08 23:20:22 +0000

    Add create new Rental functionality

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -12,6 +12,7 @@ import { RentalListItemComponent } from './rental-list-item/rental-list-item.com
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 import { RentalDetailBookingComponent } from './rental-detail/rental-detail-booking/rental-detail-booking.component';
 import { RentalSearchComponent } from './rental-search/rental-search.component';
+import { RentalCreateComponent } from './rental-create/rental-create.component';
 
 import { RentalService } from "./shared/rental.service";
 import { HelperService } from "../common/service/helper.service";
@@ -25,6 +26,7 @@ const routes: Routes = [
     component: RentalComponent,
     children: [
         { path: '', component: RentalListComponent },
+        { path: 'new', component: RentalCreateComponent, canActivate: [AuthGuard] },
         { path: ':rentalId', component: RentalDetailComponent, canActivate: [AuthGuard] },
         { path: ':city/homes', component: RentalSearchComponent }
   ]}
@@ -38,6 +40,7 @@ const routes: Routes = [
         RentalDetailComponent,
         RentalDetailBookingComponent,
         RentalSearchComponent,
+        RentalCreateComponent,
     ],
     imports: [
         CommonModule,
--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 
 import { HttpClient } from '@angular/common/http';
 
+import { Rental } from "./rental.model";
 
 @Injectable()
 export class RentalService {
@@ -18,5 +19,9 @@ export class RentalService {
 
 	public getRentalsByCity(city): Observable<any> {
 		return this.http.get(`/api/v1/rentals?city=${city}`);
+	}
+
+	public createRental(rental: Rental): Observable<any> {
+		return this.http.post('/api/v1/rentals', rental);
 	}
 }
