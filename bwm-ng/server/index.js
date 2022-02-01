73a3a153999f444592f28dbe8776c1977c6303ab
Jerel Henderson <henderson.jerel@yahoo.com>
2022-01-31 19:45:49 -0600

    Begin Heroku deployment setup

--- bwm-ng/server/index.js
+++ bwm-ng/server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db');
 
 const rentalRoutes = require('./routes/rentals');
@@ -10,8 +11,10 @@ const userRoutes = require('./routes/users');
 const bookingRoutes = require('./routes/bookings')
 
 mongoose.connect(config.DB_URI).then(() => {
+    if (process.env.NODE_EV !== 'production') {
     const fakeDb = new FakeDb();
     // fakeDb.seedDb();
+    }
 });
 
 const app = express();
@@ -20,7 +23,15 @@ app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 
+if (process.env.NODE_EV === 'production') {
+    const appPath = path.join(__dirname, '..', 'dist/browser');
+    app.use(express.static(appPath));
+    app.get('*', function(req, res) {
+        res.sendFile(path.resolve(appPath, 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log('Server started.')
 });
